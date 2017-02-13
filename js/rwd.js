eee5c4cb5b783254c2c368d9b443369b629d6d8e
Kieran McFarlane <kieran@verbbrands.com>
2017-02-13 15:26:44 +0000

    slideToggle

--- js/rwd.js
+++ js/rwd.js
@@ -291,7 +291,7 @@ rabs.find('a').off().on('click', function(e){
 
   newRab.siblings(':visible').fadeOut('fast');
 
-    newRab.slideToggle(1300, "linear");
+    newRab.slideToggle(1000, "linear");
 
 });
 
